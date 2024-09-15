e534170d63d452b0a8cdf11401884ad919937b85
Gokhan Taskan <gokhan.frontfeed@gmail.com>
2024-09-15 11:46:13 +0300

    Refactor tailwindcss-config package.json exports

--- packages/tailwindcss-config/tsup.config.ts
+++ packages/tailwindcss-config/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig(options => ({
   clean: true,
+  dts: true,
   minify: !options.watch,
   sourcemap: !!options.watch,
   format: ["esm", "cjs"],
