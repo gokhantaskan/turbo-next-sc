edaafe848a0f255bf3f7317ac9c167202c6150b0
Gokhan Taskan <gokhan.frontfeed@gmail.com>
2024-09-17 10:16:28 +0300

    refactor: remove unused code in AppNavigation component

--- apps/client/components/app/AppNavigation.tsx
+++ apps/client/components/app/AppNavigation.tsx
@@ -1,14 +1,10 @@
 import { Button } from "@repo/ui/Button";
 import Logo from "assets/img/icons/next.svg";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React from "react";
 
 import { ROUTE_ENDPOINTS } from "@/lib/constants/endpoints";
 
 export const AppNavigation = (): JSX.Element => {
-  const router = useRouter();
-
   return (
     <header className="py-4 bg-white border-b">
       <div className="container flex items-center justify-between">
